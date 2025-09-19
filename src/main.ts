f0d6ad844a09489c91dc42420159b61ed8bbbdcf
karanBhatnagar29 <karanbhatnagar862@gmail.com>
2025-09-19 22:42:48 +0530

    solved cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3001', 'http://13.202.200.98:3001/'], // your Next.js frontend
+    origin: ['http://localhost:3001', 'http://13.202.200.98:3001'], // your Next.js frontend
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
