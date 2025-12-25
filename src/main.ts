6e16f23474afe0bceca6017586141289cac5b1f4
karanBhatnagar29 <karanbhatnagar862@gmail.com>
2025-12-25 21:46:14 +0530

    cors solved

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3001', 'http://13.202.200.98:3001'], // your Next.js frontend
+    origin: [
+      'http://localhost:3001',
+      'http://13.202.200.98:3001',
+      'https://mega-rail-frontend.vercel.app',
+      'http://mega-rail-frontend.vercel.app',
+    ], // your Next.js frontend
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
